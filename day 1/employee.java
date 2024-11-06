773497705cc61214e10cde15d53f7f535d51454a
harikasrisai007 <maramshankar007@gmail.com>
2024-11-06 20:10:46 +0530

    Add files via upload

---
+++ day 1/employee.java
@@ -0,0 +1,13 @@
+class employee
+{
+public static void main(String args[])
+{
+int enumber=101;
+String ename="harika";
+float esalary=135.99f;
+
+System.out.println("given enumber:"+enumber);
+System.out.println("given ename:"+ename);
+System.out.println("given esalary:"+esalary);
+}
+}
---
+++ day 1/student.java
@@ -0,0 +1,16 @@
+class student
+{
+public static void main(String args[])
+{
+int stu_rollno=101;
+String stu_name="Karan";
+float stu_fee=135.99f;
+int stu_marks=95;
+
+System.out.println("student roll.no:"+stu_rollno);
+System.out.println("student name:"+stu_name);
+System.out.println("student fee:"+stu_fee);
+System.out.println("student marks:"+stu_marks);
+
+}
+}
